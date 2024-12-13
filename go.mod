fa60825805f5d68faf3ee73a5fc5f36d31894bae
David Odohi <odohi.david@gmail.com>
2024-12-13 02:19:16 +0000

    add cors

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/docker/go-connections v0.5.0
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/joho/godotenv v1.5.1
--- server/routes.go
+++ server/routes.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +12,14 @@ func (s *Server) RegisterRoutes() http.Handler {
 	// TODO: configure the gin logger to use slog to be consistent with the
 	// rest of the application
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // TODO: change this to the actual domain
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "OPTIONS", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/", s.HelloWorldHandler)
 
